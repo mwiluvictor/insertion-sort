461b6c9df6cef293d4e9364d6b477158f6319545
mwiluvictor <mwilumuisyo@gmail.com>
2023-11-01 21:16:47 +0300

    update

--- insertion_sort.c
+++ insertion_sort.c
@@ -1,30 +1,53 @@
 #include <stdio.h>
 
 int main(){
-	int i, j, n, temp;
+	int i, j, n, temp, choice;
 	int arr[32];
 
 	printf("Enter the number of items to sort:\n");
 	scanf("%d", &n);
 
 	printf("Enter %d integers:\n", n);
-
 	for(i=0; i < n; i++)
 		scanf("%d", &arr[i]);
 
-	for(i=1; i < n; i++){
-		j = i;
+	printf("Choose the desired sorting order:\n");
+	printf("1: Ascending order\n");
+	printf("2: Descending order\n");
+
+	scanf("%d", &choice);
+
+	if(choice == 1){
+		for(i=1; i < n; i++){
+			j = i;
 		
-		while(j > 0 && arr[j-1] > arr[j]){
-			temp = arr[j];
-			arr[j] = arr[j-1];
-			arr[j-1] = temp;
-			j--;
+			while(j > 0 && arr[j-1] > arr[j]){
+				temp = arr[j];
+				arr[j] = arr[j-1];
+				arr[j-1] = temp;
+				j--;
+			}
 		}
-	}
 	
-	for(i=0; i < n; i++)
-		printf("%d\n", arr[i]);
+		for(i=0; i < n; i++)
+			printf("%d\n", arr[i]);
+	}
+
+	if(choice == 2){
+		for(i=1; i < n; i++){
+			j = i;
+		
+			while(j > 0 && arr[j-1] < arr[j]){
+				temp = arr[j];
+				arr[j] = arr[j-1];
+				arr[j-1] = temp;
+				j--;
+			}
+		}
+	
+		for(i=0; i < n; i++)
+			printf("%d\n", arr[i]);
+	}
 
 
 	return 0;
